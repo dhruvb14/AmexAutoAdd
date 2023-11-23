9b21659cb8b62018435b42c77c48e9518ef9275f
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2023-11-23 16:49:55 -0500

    Add readme and code

---
+++ AmexAutoAddOffers.js
@@ -0,0 +1,111 @@
+function openSwitcher() {
+    var toggleCard = document.querySelectorAll('button.account-switcher-toggler');
+    toggleCard[0].click();
+}
+
+function selectGoldCard() {
+    openSwitcher()
+    var goldmenu = document.querySelector('button[aria-label^="American Express Gold Card"]');
+    goldmenu.click();
+}
+
+function selectPlatinumCard() {
+    openSwitcher()
+    var platmenu = document.querySelector('button[aria-label^="Platinum Card"]');
+    platmenu.click();
+}
+
+async function addCurrentlyVisibleOffersToCard() {
+
+    await navigateToOffers();
+    (function (i) {
+        console.log("Waiting for offers to load");
+        setTimeout(function () {
+            console.log("Lets look for and all all visible offers");
+            var buttons = document.querySelectorAll('button.offer-cta:not([href])');
+            for (var i = 0; i < buttons.length; i++) {
+                (function (i) {
+                    setTimeout(function () {
+                        buttons[i].click();
+                    }, i * randomTimeBetweenClicks());
+                })(i);
+            }
+            console.log("Added all visible offers");
+        }, 10000);
+    })(i);
+
+
+}
+
+function randomTimeBetweenClicks() {
+    const val = Math.floor(1000 + Math.random() * 5000);
+    return val;
+}
+
+function howManyRemainingOffers() {
+    var eligible = document.getElementById("ELIGIBLE");
+    var innerText = eligible.innerHTML;
+    var count = innerText.match(/\d+/)[0];
+    console.log(`There are ${count} remaining offers`)
+    return count;
+}
+function refreshOffers() {
+    selectGoldCard();
+    selectPlatinumCard();
+}
+
+async function navigateToOffers() {
+    var viewMoreOffers = document.getElementById("view-more-header");
+    viewMoreOffers.click();
+}
+
+function styleButtons(button) {
+    button.classList.add("btn", "btn-icon", "btn-sm", "icon-hover", "aa-chat-pill");
+    button.style.position = "fixed";
+    button.style.bottom = "20px";
+    button.style.left = "20px";
+    button.style.width = "200px";
+}
+
+function addButtons() {
+    var button = document.createElement("button");
+    button.classList.add("btn", "btn-icon", "btn-sm", "icon-hover", "aa-chat-pill");
+    button.style.position = "fixed";
+    button.style.bottom = "20px";
+    button.style.left = "20px";
+    button.style.width = "200px";
+    button.innerHTML = "Add All Offers";
+    button.onclick = addCurrentlyVisibleOffersToCard;
+    document.body.appendChild(button);
+
+    button = document.createElement("button");
+    button.classList.add("btn", "btn-icon", "btn-sm", "icon-hover", "aa-chat-pill");
+    button.style.position = "fixed";
+    button.style.bottom = "60px";
+    button.style.left = "20px";
+    button.style.width = "200px";
+    button.innerHTML = "Refresh All Offers";
+    button.onclick = refreshOffers;
+    document.body.appendChild(button);
+
+    button = document.createElement("button");
+    button.classList.add("btn", "btn-icon", "btn-sm", "icon-hover", "aa-chat-pill");
+    button.style.position = "fixed";
+    button.style.bottom = "100px";
+    button.style.left = "20px";
+    button.style.width = "200px";
+    button.innerHTML = "Select Gold Card";
+    button.onclick = selectGoldCard;
+    document.body.appendChild(button);
+
+    button = document.createElement("button");
+    button.classList.add("btn", "btn-icon", "btn-sm", "icon-hover", "aa-chat-pill");
+    button.style.position = "fixed";
+    button.style.bottom = "140px";
+    button.style.left = "20px";
+    button.style.width = "200px";
+    button.innerHTML = "Select Platinum Card";
+    button.onclick = selectPlatinumCard;
+    document.body.appendChild(button);
+}
+addButtons();
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AmexAutoAdd
 Amex Auto Add Offers
+
+1. Login to americanexpress.com in Chrome or Edge
+1. Right click somewher in the whitespace then click `Inspect`
+1. In the new Developer tools that show up find the tab at the top called `Console`
+1. Paste all the code from `AmexAutoAddOffers.js` into the window
+1. In the bottom left of the screen you should see 4 new buttons. 
+1. Click `Add All Offers` and just wait. 
+
+It takes about 10 seconds before it start working. After it inevitably stops adding offers you can click `Home` on the website address bar then click `Add All Offers` again until no more offers show up.
